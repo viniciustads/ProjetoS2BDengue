c10900a6575692ce0f11a9f114acb172a5d53db6
clawdyvan <clawdyvan@hotmail.com>
2015-05-28 23:16:53 -0300

    Início do Projeto

---
+++ ProjetoS2BDengue.Windows/Tela/PaginaPrincipal.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using ProjetoS2BDengue.Data;
+using ProjetoS2BDengue.Common;
+
+namespace ProjetoS2BDengue
+{
+    public sealed partial class PaginaPrincipal : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        public PaginaPrincipal()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            //this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
+        }
+    }
+}
---
+++ ProjetoS2BDengue.WindowsPhone/Telas/PaginaPrincipal.xaml.cs
@@ -0,0 +1,69 @@
+﻿using ProjetoS2BDengue.Common;
+using ProjetoS2BDengue.Data;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Resources;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace ProjetoS2BDengue
+{
+
+    public sealed partial class PaginaPrincipal : Page
+    {
+        private readonly NavigationHelper navigationHelper;
+        private readonly ObservableDictionary defaultViewModel = new ObservableDictionary();
+        private readonly ResourceLoader resourceLoader = ResourceLoader.GetForCurrentView("Resources");
+
+        public PaginaPrincipal()
+        {
+            this.InitializeComponent();
+
+            // Hub is only supported in Portrait orientation
+            DisplayInformation.AutoRotationPreferences = DisplayOrientations.Portrait;
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            this.navigationHelper = new NavigationHelper(this);
+            //this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
+            //this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
+
+            
+        }
+
+        private void MovieTrailers_Loaded(object sender, RoutedEventArgs e)
+        {
+            var grid = (Grid)sender;
+            grid.Children.Add(new Telas.PaginaHome());
+            
+        }
+
+
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+    }
+}
